64ee3cdced4d1223a70cc5bd07483e0cf977ad5f
tksgtkm <h9cd342fd7jgx36@gmail.com>
2021-12-02 22:59:59 +0900

    browse test

---
+++ backend/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>トップ画面</title>
+  </head>
+  <body>
+    <p>
+      草
+<!-- @if (Auth::check())
+  {{ \Auth::user()->name}}さん
+  <p> <a href="/logout">ログアウト</a> </p>
+@else
+  ゲストさん -->
+  　</p>
+  <!-- <p> <a href="/login">ログイン</a> <br> <a href="/register">会員登録</a> </p>
+@endif -->
+  </body>
+</html>
